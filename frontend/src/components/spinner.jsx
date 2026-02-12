6f8b09f4b77b4b89a455a608ce0d94307b7956cb
SakshiRathod13 <sakshirathod84352@gmail.com>
2026-02-12 21:35:13 +0530

    Frontend Added

--- frontend/src/components/spinner.jsx
+++ frontend/src/components/spinner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const spinner = () => {
   return (
     <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'>
-        spinner
+        Spinner
       
     </div>
   )
